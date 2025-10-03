93186cce3084168cc719a0fbf9db879aadd31d8c
Sandirane <kumarasamy.sandirane.77@gmail.com>
2025-10-03 17:18:21 +0200

    add bootstrap

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Home } from './feature/pages/home/home';
+import { ExamsList } from './feature/pages/exams-list/exams-list';
+import { Pagenotfound } from './feature/pages/pagenotfound/pagenotfound';
+
+export const routes: Routes = [
+
+  { path: 'home', component: Home },
+  { path: 'exams-list', component: ExamsList },
+
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', component: Pagenotfound },
+  
+];
---
+++ frontend/src/app/feature/pages/exams-list/exams-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exams-list',
+  imports: [],
+  templateUrl: './exams-list.html',
+  styleUrl: './exams-list.css'
+})
+export class ExamsList {
+
+}
