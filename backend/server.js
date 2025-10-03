07aa8eaab5588d5783b97c09f41c88a46cbcad87
Sandirane <kumarasamy.sandirane.77@gmail.com>
2025-10-03 16:54:59 +0200

    fix sawwger API path

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,17 @@ const cors = require('cors');
 app.use(bodyParser.json());
 app.use(cors());
 
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const path = require('path');
+
+const swaggerDocument = YAML.load(path.join(__dirname, 'exam-api.yml'));
+
+app.use(bodyParser.json());
+app.use(cors());
+ 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 let items = [];
 
 app.get('/api/exams', (req, res) => {
